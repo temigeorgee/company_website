4ddbd903f854fa2574faafdd5473dbaf4c627f54
temigeorgee <george.temidayo@gmail.com>
2021-03-04 00:11:10 +0100

    deleted file

--- src/components/services.js
+++ src/components/services.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function services() {
-    return (
-        <div>
-            <div className="services__section">
-                <div className="services__wrapper">
-                    <h1 className="services__heading">
-                        Services
-                    </h1>
+  return (
+    <div>
+      <div className="services__section">
+        <div className="services__wrapper">
+          <h1 className="services__heading">Services</h1>
+          <div className="services__container">
+            <div className="services__container-cardInfo">
+              <div className="services__container__img">
+                <img src="https://images.unsplash.com/photo-1574607383476-f517f260d30b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80" alt="" />
+              </div>
+              <div className="services__content">
+                <div className="services__title">
+                  <h3>Fast Loan</h3>
                 </div>
+                <div className="services__body">
+                  <h3></h3>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default services
+export default services;
