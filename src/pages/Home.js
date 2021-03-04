fb13611515d0e3cf0546ca90dafdf24e882a8d2c
temigeorgee <george.temidayo@gmail.com>
2021-03-04 15:00:06 +0100

    removed services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,13 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import Offer from '../components/Offer'
 import Pricing from '../components/Pricing'
-import Services from '../components/Services'
+// import Services from '../components/Services'
 import{ homeObjThree } from './data'
 
 function Home() {
     return (
         <>
           <HeroSection />
-          <Services/>
           <Offer {...homeObjThree}/>
           <Pricing/>
 
